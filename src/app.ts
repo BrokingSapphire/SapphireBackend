d94c823e3973403e5845bf991451d5f111dfbf3d
Abhigya Krishna <abhigyakrishna21@gmail.com>
2025-03-15 00:31:14 +0530

    fix: body parser

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 import helmet from 'helmet';
 import cors from 'cors';
 import csurf from 'csurf';
+import bodyParser from 'body-parser';
 import { routeLogger, errorLogger, responseCapture, errorHandler } from '@app/middlewares';
 import expressWs from 'express-ws';
 import { initializeRedis, closeRedisConnection } from '@app/services/redis.service';
@@ -17,8 +18,8 @@ expressWs(app);
 
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Setup logging middleware
 app.use(responseCapture);
